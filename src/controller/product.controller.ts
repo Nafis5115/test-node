fd02792a6582d3450ff7e1f094667d9aaf83a3a2
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-05-22 11:15:07 +0600

    creating post api

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,13 +1,17 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import { parseBody } from "../utils/parseBody";
 
-export const productController = (
+export const productController = async (
   req: IncomingMessage,
   res: ServerResponse,
 ) => {
   const url = req.url;
   const method = req.method;
   const products = readProduct();
+  const urlParts = url?.split("/");
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
 
   if (url === "/products" && method === "GET") {
     res.writeHead(200, { "content-type": "application/json" });
@@ -17,5 +21,31 @@ export const productController = (
         products: products,
       }),
     );
+  } else if (method === "GET" && id !== null) {
+    const product: { id: number; name: string; price: number } = products.find(
+      (product: { id: number; name: string; price: number }) =>
+        product.id === id,
+    );
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "THIs is single product route",
+        product,
+      }),
+    );
+  } else if (url === "/products" && method === "POST") {
+    let body = await parseBody(req);
+    console.log(body);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Successfully created a product",
+        body,
+      }),
+    );
+  } else {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("404 not found");
   }
 };
