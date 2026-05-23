7c342c2a0eea0fee35ffdf4dbce6d3ee39ddb296
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-05-23 12:30:28 +0600

    complete delete method

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -52,15 +52,7 @@ export const productController = async (
   } else if (method === "PUT" && id !== null) {
     const body = await parseBody(req);
     const index = products.findIndex((p) => p.id === id);
-    if (index < 0) {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(
-        JSON.stringify({
-          message: "product not found",
-          data: null,
-        }),
-      );
-    }
+
     products[index] = {
       id: products[index].id,
       ...body,
@@ -70,6 +62,26 @@ export const productController = async (
     res.end(
       JSON.stringify({
         message: "Successfully updated a product",
+        product: products[index],
+      }),
+    );
+  } else if (method === "DELETE" && id !== null) {
+    const index = products.findIndex((p) => p.id === id);
+    if (index < 0) {
+      res.writeHead(200, { "content-type": "application/json" });
+      return res.end(
+        JSON.stringify({
+          message: "product not found",
+          data: null,
+        }),
+      );
+    }
+    products.splice(index, 1);
+    insertProduct(products);
+    res.writeHead(200, { "content-type": "application/json" });
+    return res.end(
+      JSON.stringify({
+        message: "Successfully deleted a product",
         product: products[index],
       }),
     );
